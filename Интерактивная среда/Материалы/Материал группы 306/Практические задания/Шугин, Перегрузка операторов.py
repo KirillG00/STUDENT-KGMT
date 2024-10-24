642f8a6d03fe92271535976ec14e03e41d707dd2
BotAdmin-coderITSE <superbot367@outlook.com>
2024-10-24 09:01:17 +0300

    vault backup from user KGMT-BOT-26: 2024-10-24 (09:01:17–13:21:59
    vault backup from user KGMT-BOT-26: 2024-10-24 09:01:17
    
    vault backup from user KGMT-BOT-26: 2024-10-24 09:11:17
    
    vault backup from user KGMT-BOT-26: 2024-10-24 09:21:20
    
    vault backup from user KGMT-BOT-26: 2024-10-24 09:41:12
    
    vault backup from user KGMT-BOT-26: 2024-10-24 10:41:13
    
    vault backup from user KGMT-BOT-26: 2024-10-24 10:51:12
    
    vault backup from user KGMT-BOT-26: 2024-10-24 11:51:12
    
    vault backup from user KGMT-BOT-26: 2024-10-24 12:01:16
    
    vault backup from user KGMT-BOT-26: 2024-10-24 12:11:02
    
    vault backup from user KGMT-BOT-26: 2024-10-24 12:21:06
    
    vault backup from user KGMT-BOT-26: 2024-10-24 12:31:02
    
    vault backup from user KGMT-BOT-26: 2024-10-24 12:41:06
    
    vault backup from user KGMT-BOT-26: 2024-10-24 13:21:59

---
+++ Интерактивная среда/Материалы/Материал группы 306/Практические задания/Шугин, Перегрузка операторов.py
@@ -0,0 +1,30 @@
+def sum_of_integers(numbers):
+    for number in numbers:
+        # Проверяем на чётное число
+        if number % 2 == 0:
+            raise ValueError("В списке не должно быть чётных чисел")
+        # Проверяем на отрицательное число
+        if number < 0:
+            raise ValueError("В списке не должно быть отрицательных чисел")
+
+    # Если список прошёл проверку, возьмём его сумму
+    return sum(numbers)
+
+
+# Примеры использования
+try:
+    numbers1 = [1, 3, 5, 7, 9]
+    print(sum_of_integers(numbers1))  # Вывод: 25
+
+    numbers2 = [1, 2, 5, 7, 9]
+    print(sum_of_integers(numbers2))  # В этом случае будет вызвано исключение
+
+except ValueError as e:
+    print(e)
+
+try:
+    numbers3 = [1, -2, 5, 7, 9]
+    print(sum_of_integers(numbers3))  # В этом случае также будет вызвано исключение
+
+except ValueError as e:
+    print(e)
